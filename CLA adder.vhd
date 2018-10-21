48d6879c906386c6207eb1cf2d82c17f93b26f63
sajad hamzenejadi <40471014+sajadh76@users.noreply.github.com>
2018-10-21 07:52:13 +0330

    Add files via upload

---
+++ CLA adder.vhd
@@ -0,0 +1,38 @@
+library ieee;
+use ieee.std_logic_1164.all;
+entity carry_lookahead_adder is
+port (
+i_add1 : in std_logic_vector(15 downto 0);
+i_add2 : in std_logic_vector(15 downto 0);
+o_result : out std_logic_vector(16 downto 0)
+);
+end carry_lookahead_adder;
+architecture simple of carry_lookahead_adder is
+component full_adder is
+port (
+a : in std_logic;
+b : in std_logic;
+cin : in std_logic;sum : out std_logic;
+cout : out std_logic
+);
+end component full_adder;
+signal Generat: std_logic_vector(15 downto 0); -- Generate
+signal Propagate: std_logic_vector(15 downto 0); -- Propagate
+signal Cin: std_logic_vector(16 downto 0); -- Carry
+signal Sum : std_logic_vector(15 downto 0);
+begin
+-- Create the Full Adders
+GEN_FULL_ADDERS : for ii in 0 to 15 generate
+FULL_ADDER_INST : full_adder
+port map (a => i_add1(ii), b => i_add2(ii), cin => Cin(ii), sum => Sum(ii), cout => open);
+end generate GEN_FULL_ADDERS;-- Gi=Ai*Bi
+-- Pi=Ai+Bi
+-- Create the Carry Terms
+GEN_CLA : for jj in 0 to 15 generate
+Generat (jj) <= i_add1(jj) and i_add2(jj);
+Propagate (jj) <= i_add1(jj) or i_add2(jj);
+Cin(jj+1) <= Generat (jj) or (Propagate (jj) and Cin(jj)); ----Cin = Pi or Gi
+end generate GEN_CLA;
+Cin(0) <= '0'; -- no carry input
+o_result <= Cin(16) & Sum ;
+end simple;
---
+++ testbench-for-CLA-adder.vhd
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;entity carry_lookahead_adder_tb is
+end carry_lookahead_adder_tb;
+architecture behave of carry_lookahead_adder_tb is
+signal r_ADD_1 : std_logic_vector(15 downto 0) := (others => '0');
+signal r_ADD_2 : std_logic_vector(15 downto 0) := (others => '0');
+signal w_RESULT : std_logic_vector(16 downto 0);
+component carry_lookahead_adder is
+port (
+i_add1 : in std_logic_vector(15 downto 0);
+i_add2 : in std_logic_vector(15 downto 0);
+o_result : out std_logic_vector(16 downto 0)
+);
+end component carry_lookahead_adder;
+begin
+CLAtest : carry_lookahead_adder port map (
+i_add1 => r_ADD_1,
+i_add2 => r_ADD_2,
+o_result => w_RESULT
+);process is
+begin
+r_ADD_1 <= "0000000000000000";
+r_ADD_2 <= "0000000000000000";
+wait for 20 ns;
+r_ADD_1 <= "1111111111111111";
+r_ADD_2 <= "1111111111111111";
+wait for 20 ns;
+r_ADD_1 <= "0000001111110011";
+r_ADD_2 <= "0010111000100011";
+wait for 20 ns;
+end process;
+end behave;
